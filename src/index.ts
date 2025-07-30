a491fe7cdfd3537e1d237f9669a17c952eb89d0a
szzdmj <chuanfuyin001@gmail.com>
2025-07-31 07:22:00 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { createPHP } from "@php-wasm/web";
+
 export default {
   async fetch(req: Request): Promise<Response> {
-    const php = new PHP({
+    const php = await createPHP({
       print: (text) => console.log("PHP output:", text),
     });
 
-    await php.mount({ "index.php": <?php echo "Hello from PHP running inside Cloudflare Worker!"; ?> });
+    await php.mount({
+      "index.php": `<?php echo "Hello from PHP running inside Cloudflare Worker!"; ?>`,
+    });
 
     const result = await php.run("index.php");
 
