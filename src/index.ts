d328069a4d194823178667a914e2dbd7b69e1f52
szzdmj <chuanfuyin001@gmail.com>
2025-08-01 23:28:58 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import phpWasm from "@php-wasm/web";
+
 export default {
   async fetch(req: Request): Promise<Response> {
-    const php = new PHP({
+    const php = await phpWasm({
       print: (text) => console.log("PHP output:", text),
     });
 
-    await php.mount({ "index.php": `<?php echo "Hello from PHP running inside Cloudflare Worker!"; ?>` });
+    await php.mount({
+      "index.php": `<?php echo "Hello from PHP running inside Cloudflare Worker!"; ?>`,
+    });
 
+    // 无论访问什么路径，都运行 index.php
     const result = await php.run("index.php");
 
     return new Response(result.stdout, {
