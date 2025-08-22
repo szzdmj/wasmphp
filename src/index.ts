2dc4d0d956146efc3b2ecbeaf72e31ff0ea99ae8
szzdmj <chuanfuyin001@gmail.com>
2025-08-22 20:41:28 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-// Only export asyncify 8.4 factory, minimal build for Cloudflare Worker!
-module.exports = require('./php/asyncify/8_4_10/php_8_4.js');
+import * as phpWasm from "@php-wasm/web";
+
+export default {
+  async fetch(req: Request): Promise<Response> {
+    const php = await phpWasm.default({
+      print: (text: string) => console.log("PHP output:", text),
+    });
+
+    await php.mount({ "index.php": `<?php echo "Hello from PHP running inside Cloudflare Worker!"; ?>` });
+
+    const result = await php.run("index.php");
+
+    return new Response(result.stdout, {
+      headers: { "content-type": "text/html; charset=utf-8" },
+    });
+  },
+};
