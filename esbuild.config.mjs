12b8515af2c2cc4f2bff49a6fbb804bb2067ed24
szzdmj <chuanfuyin001@gmail.com>
2025-08-23 06:32:46 +0800

    Update esbuild.config.mjs

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -7,6 +7,11 @@ build({
   platform: "browser",
   target: "esnext",
   loader: { ".dat": "file", ".wasm": "file" },
+  external: [
+    "@php-wasm/web/php/asyncify/8_4_10/php_8_4.js",
+    "@php-wasm/web/php/asyncify/8_4_10/php_8_4.wasm",
+    "@php-wasm/web/shared/icudt74l.dat"
+  ],
   plugins: [
     {
       name: "node-core-shim",
