bc70c6431ffa37bbd4c4f789eb27c76ab1e58226
szzdmj <chuanfuyin001@gmail.com>
2025-08-22 22:40:23 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as phpWasmModule from "@php-wasm/web/php/asyncify/8_4_10/php_8_4.js";
 export default {
   async fetch(req: Request): Promise<Response> {
     const php = await phpWasmModule.init(
-      "WORKER", // 推荐用 "WORKER" 环境，Cloudflare Worker 专用
+      "WORKER",
       {
         print: (text: string) => console.log("PHP output:", text),
       }
