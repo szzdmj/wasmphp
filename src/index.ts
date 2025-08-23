f29e6edaffeb138f6edced099d6551d0dd2c2f05
szzdmj <chuanfuyin001@gmail.com>
2025-08-23 11:13:50 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
-import { init } from "../scripts/php_8_4.js";
+import { init, dependencyFilename } from '../scripts/php_8_4.js';
 
 export default {
-  async fetch(request: Request) {
-    const php = await init("WORKER"); // 只需传 "WORKER"，其余参数可省略，除非你有自定义需求
-
-    await php.mount({
-      "index.php": `<?php echo "Hello from PHP WASM!";`
-    });
-
-    const result = await php.run("index.php");
-
-    return new Response(result.stdout, {
-      headers: { "content-type": "text/html; charset=utf-8" }
-    });
+  async fetch(request, env, ctx) {
+    const phpLoader = {};
+    // dependencyFilename 是 Uint8Array
+    phpLoader['wasmBinary'] = dependencyFilename;
+    await init("WORKER", phpLoader);
+    return new Response("WASM PHP initialized!", { status: 200 });
   }
-};
+}
