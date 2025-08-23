9de9518f682b8422f97d2b8d5024160c9ed546da
szzdmj <chuanfuyin001@gmail.com>
2025-08-23 09:29:03 +0800

    Refactor fetch method and enhance response headers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { init } from "@php-wasm/web";
+
 export default {
-  async fetch(request: Request, env: any, ctx: any) {
+  async fetch(request: Request) {
+    // 指定静态资源路径
     const php = await init({
       locateFile: (file: string) => {
         if (file.endsWith('.wasm')) return '/scripts/php_8_4.wasm';
@@ -9,10 +11,15 @@ export default {
       },
       print: (text: string) => console.log("PHP output:", text),
     });
+
+    // 挂载 index.php
     await php.mount({
       "index.php": `<?php echo "Hello from PHP WASM!";`
     });
     const result = await php.run("index.php");
-    return new Response(result.stdout);
+
+    return new Response(result.stdout, {
+      headers: { "content-type": "text/html; charset=utf-8" }
+    });
   }
 };
