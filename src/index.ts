2489bd6a52422069f392a6132c97d8b325f53024
szzdmj <chuanfuyin001@gmail.com>
2025-08-23 11:22:57 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { init, dependencyFilename } from '../scripts/php_8_4.js';
 
 export default {
   async fetch(request, env, ctx) {
-    const phpLoader = {};
-    // dependencyFilename 是 Uint8Array
-    phpLoader['wasmBinary'] = dependencyFilename;
-    await init("WORKER", phpLoader);
-    return new Response("WASM PHP initialized!", { status: 200 });
+    // 输出类型与内容
+    return new Response(
+      `typeof dependencyFilename: ${typeof dependencyFilename}; is Uint8Array: ${dependencyFilename instanceof Uint8Array}`,
+      { status: 200 }
+    );
   }
 }
